286c338789261e9f004953b48bbac18ee78c3cf0
AlphonseDesign <snipermurdeshommesmuhavuweka@gmail.com>
2025-10-02 03:32:33 -0800

    FIX: Ajout de l'URL Render à ALLOWED_HOSTS

--- carnet/settings.py
+++ carnet/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-umi10be49hr*wqhvaasn@uu!dgf7+^0y$tr@=hu9iv+0r5am1_'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carnet-contacts.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
